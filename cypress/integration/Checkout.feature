e5005507d21c2ec8ddcf58d4970624ab24d2c48b
maxxyp <davin.riches@gmail.com>
2021-09-20 19:38:24 +0100

    added checkout scenario

---
+++ cypress/integration/Checkout.feature
@@ -0,0 +1,16 @@
+Feature: Checkout
+
+  Scenario: As a user I want to checkout successfully
+    Given I am on the home page
+    And I click on the signin link
+    And I enter my email adress
+    And I enter my password
+    When I click on the signin button
+    And I search for dress
+    And I add item to cart
+    And proceed to checkout
+    And I accept terms and conditions and proceed to checkout
+#    And I choose check as payment method
+#    And I click confirm order
+#    Then I verify order is complete and successful
+#    And I log out
--- cypress/integration/step_definitions/loginsteps.js
+++ cypress/integration/step_definitions/loginsteps.js
@@ -24,3 +24,31 @@ Then(/^I verify that I am logged in successfully$/, function () {
 Then(/^I log out$/, function () {
     cy.get('.logout').click()
 });
+When(/^I search for dress$/, function () {
+    cy.get('#search_query_top').type('dress')
+    cy.get('#searchbox > .btn').click()
+});
+
+When(/^I add item to cart$/, function () {
+    cy.get(':nth-child(1) > .product-container > .right-block > .button-container > .ajax_add_to_cart_button > span').click()
+});
+
+When(/^proceed to checkout$/, function () {
+    cy.get('.button-container > .button-medium > span').click()
+    cy.get('.cart_navigation > .button > span').click()
+    cy.get('.cart_navigation > .button > span').click()
+});
+When(/^I accept terms and conditions and proceed to checkout$/, function () {
+    cy.get('#cgv').click()
+    cy.get('.cart_navigation > .button > span').click()
+});
+
+When(/^I choose check as payment method$/, function () {
+
+});
+When(/^I click confirm order$/, function () {
+
+});
+Then(/^I verify order is complete and successful$/, function () {
+
+});
